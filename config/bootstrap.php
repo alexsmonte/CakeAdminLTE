681d29c45b44b11a52a1f8134f34c51d8e76a23e
Alex Monte <alexsmonte@gmail.com>
2017-05-16 16:06:31 -0400

    Starting the work

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -9,16 +9,44 @@ if (!Configure::read('CakeAdminLTE.projectNameMini')) {
     Configure::write('CakeAdminLTE.projectNameMini', 'A-LTE');
 }
 
-
 if (!Configure::read('CakeAdminLTE.description')) {
-    Configure::write('CakeAdminLTE.description', 'CakePHP: the rapid development php framework');
+    Configure::write('CakeAdminLTE.description', 'CakePHP AdminLTE Theme: the rapid development php framework');
 }
 
 if (!Configure::read('CakeAdminLTE.version')) {
     Configure::write('CakeAdminLTE.version', '2.3.8');
 }
+
 if (!Configure::read('CakeAdminLTE.copyright')) {
     Configure::write('CakeAdminLTE.copyright', 'Copyright &copy; 2014-2016');
+}
+
+if (!Configure::read('CakeAdminLTE.showSidebarConfig')) {
+    Configure::write('CakeAdminLTE.showSidebarConfig',false);
+}
+
+if (!Configure::read('CakeAdminLTE.showNavbar')) {
+    Configure::write('CakeAdminLTE.showNavbar',false);
+}
+
+if (!Configure::read('CakeAdminLTE.showFormSearch')) {
+    Configure::write('CakeAdminLTE.showFormSearch',false);
+}
+
+if (!Configure::read('CakeAdminLTE.showSidebarUser')) {
+    Configure::write('CakeAdminLTE.showSidebarUser',true);
+}
+
+if (!Configure::read('CakeAdminLTE.showSidebarMenu')) {
+    Configure::write('CakeAdminLTE.showSidebarMenu',true);
+}
+
+if (!Configure::read('CakeAdminLTE.showUserAccount')) {
+    Configure::write('CakeAdminLTE.showUserAccount',true);
+}
+
+if (!Configure::read('CakeAdminLTE.showBrand')) {
+    Configure::write('CakeAdminLTE.showBrand',true);
 }
 
 
